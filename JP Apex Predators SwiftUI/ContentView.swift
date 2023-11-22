95f4d277adb740691252960ffcb421be3087c279
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-11-22 08:54:25 -0600

    Initial Commit

---
+++ JP Apex Predators SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JP Apex Predators SwiftUI
+//
+//  Created by Italo Stuardo on 22/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JP Apex Predators SwiftUI/JP_Apex_Predators_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_Predators_SwiftUIApp.swift
+//  JP Apex Predators SwiftUI
+//
+//  Created by Italo Stuardo on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_Predators_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
